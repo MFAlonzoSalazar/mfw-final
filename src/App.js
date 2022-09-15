3c97cc4da9a9bf02130627812a723888a331f50b
Fernanda <mfalonzosalazar@gmail.com>
2022-09-14 23:05:56 -0700

    trying to create a success page for the contact me

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
             <Route path='/aboutme' element={<AboutMe />} />
             <Route path='/projects' element={<Projects />} />
             <Route path='/contactme' element={<ContactMe/>}>
-              <Route path='/contactme/:success' element={<Success />} />
+              <Route path='/contactme/:id' element={<Success />} />
             </Route>
           </Route>
         </Routes>
